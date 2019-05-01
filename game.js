5c306fe8f9f81bb79c4588e5b65a63ae8c61a79b
Vercyx <49323763+Vercyx@users.noreply.github.com>
2019-05-01 09:01:06 -0400

    oo
    ooo

--- game.js
+++ game.js
@@ -1,3 +1,25 @@
+function setup() {
+  let chars = 'abcdefghijklmnopqrstuvwxyz ';
+  let pop = [];
+  let DNA = [];
+  let target = 'to be or not to be';
+  let members = 10;
+  let result = '';
+  
+  for(i = 0; i < members; i++){
+    for(z = 0; z < target.length; z++){
+      DNA.push(chars.charAt(Math.floor(Math.random() * chars.length)))
+    }
+    
+    
+    
+    pop.push(DNA);
+    console.log(DNA);
+  }
+  
+  createCanvas(400, 400);
+}
+
 function draw() {
-  console.log("heelo");
+  background(220);
 }
