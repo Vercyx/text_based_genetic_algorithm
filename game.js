ff2dc083ac0d23a72e3d9aa4d0cffdecb6f5f7d9
Vercyx <49323763+Vercyx@users.noreply.github.com>
2019-05-01 15:43:21 -0400

    Added fitness evaluation and mating pool creation

--- game.js
+++ game.js
@@ -1,23 +1,56 @@
+let chars = 'abcdefghijklmnopqrstuvwxyz ';
+let pop = [];
+let DNA = [];
+let target = 'to be or not to be';
+let targetArr = target.split('');
+let members = 10;
+let result = '';
+let fit = 0
+let fitnessVal = [];
+let matingPool = [];
+
 function setup() {
-  let chars = 'abcdefghijklmnopqrstuvwxyz ';
-  let pop = [];
-  let DNA = [];
-  let target = 'to be or not to be';
-  let members = 10;
-  let result = '';
   
   for(i = 0; i < members; i++){
-    for(z = 0; z < 18; z++){
+    for(z = 0; z < target.length; z++){
       DNA.push(chars.charAt(Math.floor(Math.random() * chars.length)));
     }
     pop.push(DNA);
     DNA = [];
-    console.log(pop);
   }
   
   createCanvas(400, 400);
+  console.log(pop)
+  evalFit();
+  addToPool();
 }
 
 function draw() {
   background(220);
+}
+
+
+
+function evalFit(){
+  for(i = 0; i < pop.length; i++){
+    for(k = 0; k < targetArr.length; k++){
+      if(pop[i][k] == targetArr[k]){
+         fit++
+         }
+    }
+    fitnessVal.push(fit);
+    fit = 0
+  }
+  console.log(fitnessVal);
+}
+
+
+function addToPool(){
+ for(i = 0; i < fitnessVal.length; i++){
+   for(k = 0; k < fitnessVal[i]; k++){
+     matingPool.push(pop[i]);
+   }
+   
+ }
+console.log(matingPool);
 }
